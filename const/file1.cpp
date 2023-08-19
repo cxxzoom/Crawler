e3ff3bf5752a79246da3b256e79de9918d6bda97
ucdo <phpsarc@gmail.com>
2023-08-19 18:00:03 +0800

    Update file1.cpp

--- const/file1.cpp
+++ const/file1.cpp
@@ -3,15 +3,17 @@
 //
 
 #include<iostream>
+
 using namespace std;
 extern int ext;
 extern const int const_e;
-int main(){
+
+int main() {
     std::cout << ext + 12 << std::endl;
     std::cout << const_e << std::endl;
 
     int a = 2;
-    const int* ptr1 = &a; //
+    const int *ptr1 = &a; //
     cout << &ptr1;
 //    *ptr1 = 1;
     ptr1 = nullptr;
